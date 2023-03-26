6db3eccbd077c94a91f74a748716218ae6bc80ff
kyle-keim <kyle.keim777@gmail.com>
2023-03-26 13:31:07 -0700

    Trying out a python CI workflow with yml file

---
+++ Databasing/tests/test_addition.py
@@ -0,0 +1,10 @@
+import unittest
+import lib.TestCode
+
+class testing1(unittest.TestCase):
+    def testAddition(self):
+        self.assertEqual(lib.TestCode.addTogether(3,4),7)
+        
+    def testFails(self):
+        self.assertNotEqual(lib.TestCode.addTogether(3,4),7)
+
