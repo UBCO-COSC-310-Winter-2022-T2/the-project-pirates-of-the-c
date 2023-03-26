9c96fdd116f268b21b8dc278ee857aaa80044ce0
kyle-keim <kyle.keim777@gmail.com>
2023-03-26 14:08:21 -0700

    added an empty __init__.py file to tests

--- Databasing/tests/test_addition.py
+++ Databasing/tests/test_addition.py
@@ -1,10 +1,10 @@
 import unittest
-import lib.TestCode
+from lib.TestCode import *
 
 class testing1(unittest.TestCase):
     def testAddition(self):
-        self.assertEqual(lib.TestCode.addTogether(3,4),7)
+        self.assertEqual(addTogether(3,4),7)
         
     def testFails(self):
-        self.assertNotEqual(lib.TestCode.addTogether(4,4),7)
+        self.assertNotEqual(addTogether(4,4),7)
 
