729f014f2b5a65f8b632444507a9795be9532dd9
rtschrit <ryan@tschritter.ca>
2023-04-03 16:33:00 -0700

    Added more tests python files

--- SimSensor/simsensor.py
+++ SimSensor/simsensor.py
@@ -1,2 +1,18 @@
-def numgen():
-        return
+class simsensor:
+        def main():
+                return
+
+        def brokerconnect():
+                return
+        
+        def brokerdisconnect():
+                return
+        
+        def clientinstance():
+                return
+
+        def numgen():
+                return
+        
+        if __name__ == "__main__":
+                main()
