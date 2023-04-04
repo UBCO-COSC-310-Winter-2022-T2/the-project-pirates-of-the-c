2f0d9555df81ee65d0c0065470db7a50cd3c5886
rtschrit <ryan@tschritter.ca>
2023-04-03 22:23:46 -0700

    Began programing the simulated sensor

--- SimSensor/simsensor.py
+++ SimSensor/simsensor.py
@@ -1,5 +1,10 @@
+import paho.mqtt.client as mqtt
+
 class simsensor:
         def main():
+                broker_address = 'eclipse-mosquitto' # The name of the docker container (hostname of the broker)
+                
+                
                 return
 
         def brokerconnect():
@@ -9,6 +14,7 @@ class simsensor:
                 return
         
         def createclient():
+                mqtt_client = mqtt.Client(client_id="", clean_session=True, userdata=None, protocol=MQTTv5, transport="tcp")
                 return
 
         def numgen():
