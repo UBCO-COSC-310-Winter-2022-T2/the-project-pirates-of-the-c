cd461edc796cd135c1224edd1214bc5ed12af6c2
rtschrit <ryan@tschritter.ca>
2023-04-11 21:46:18 -0700

    Completed numgen and fixed syntax error in test

--- SimSensor/simsensor.py
+++ SimSensor/simsensor.py
@@ -1,20 +1,26 @@
 import paho.mqtt.client as mqtt
+import random, time, math
 
-class simsensor:
-        def main():
-                return
+def main():
+        return
 
-        def brokerconnect():
-                return
-        
-        def brokerdisconnect():
-                return
-        
-        def createclient():
-                return
+def brokerconnect():
+        return
 
-        def numgen():
-                return
-        
-        if __name__ == "__main__":
-                main()
+def brokerdisconnect():
+        return
+
+def createclient():
+        return
+
+def numgen():
+        current_time = time.localtime()
+        minutes_passed = (current_time.tm_hour*60) + current_time.tm_min # The number of minutes passed in the day
+        angle = (minutes_passed*2*math.pi) / 1440 # Convert minutes to an angle between 0 and 2pi
+        phase = -2*math.cos(angle)+20 # Convert the angle to a sine function between 18 and 22 thoughout the day
+        noise = random.randint(950,1050)/1000 # Create a coefficient that scales the phase
+        temp = phase * noise # Multiply the phase by the noise to get the final random temp   
+        return temp
+
+if __name__ == "__main__":
+        main()
