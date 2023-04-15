1d297d7e448f45e036a26ef19367492aa40f541e
MehulRaisingh <mehul.raisingh@gmail.com>
2023-04-14 18:35:55 -0800

    bp test 2
    resvolved some bugs with the testing

--- Databasing/lib/bandpassfilterTEST.py
+++ Databasing/lib/bandpassfilterTEST.py
@@ -5,28 +5,30 @@ from scipy.signal import butter, filtfilt, welch
 
 from bandpassfilter import bbfilter
 
-class TestBBFilter(unittest.TestCase):
-    def setUp(self):
+def setUp(self):
         # Generate a test signal with frequencies ranging from 0 to 1000 Hz
         self.fs = 2000
         t = np.linspace(0, 1, self.fs, endpoint=False)
         self.x = np.sin(2*np.pi*50*t) + np.sin(2*np.pi*150*t) + np.sin(2*np.pi*500*t) + np.sin(2*np.pi*800*t)
 
-    def test_bandpass_filter(self):
+def test_bandpass_filter(self):
         # Apply the bandpass filter
         lowcut = 80
         highcut = 600
         y = bbfilter(self.x, self.fs, lowcut, highcut)
 
         # Check that the output signal has the expected length and shape
-        self.assertEqual(len(y), len(self.x))
-        self.assertEqual(y.ndim, 1)
+        if self.assertEqual(len(y), len(self.x)) & self.assertEqual(y.ndim, 1):
+               print("Success")
+
+        else:
+               print("fail")
 
         # Check that the output signal is within the specified frequency range
         f, P = welch(y, self.fs, nperseg=1024)
         self.assertTrue(np.all((f >= lowcut) & (f <= highcut)))
 
-    def test_highpass_filter(self):
+def test_highpass_filter(self):
         # Apply the highpass filter
         lowcut = 100
         highcut = None
@@ -40,7 +42,7 @@ class TestBBFilter(unittest.TestCase):
         f, P = welch(y, self.fs, nperseg=1024)
         self.assertTrue(np.all(f >= lowcut))
 
-    def test_lowpass_filter(self):
+def test_lowpass_filter(self):
         # Apply the lowpass filter
         lowcut = None
         highcut = 400
@@ -53,3 +55,8 @@ class TestBBFilter(unittest.TestCase):
         # Check that the output signal has no frequencies above the specified cutoff frequency
         f, P = welch(y, self.fs, nperseg=1024)
         self.assertTrue(np.all(f <= highcut))
+
+
+def main():
+    setUp()
+    test_bandpass_filter()
